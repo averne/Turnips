6b2d29467908998df0fc7096aeba41cb5aaa7327
averne <averne381@gmail.com>
2020-04-28 13:56:08 +0200

    Add dark background, tweak colors, edit readme

--- src/theme.hpp
+++ src/theme.hpp
@@ -19,30 +19,32 @@ enum class Theme {
 };
 
 static inline void apply_theme(Theme theme) {
+    auto *colors = im::GetStyle().Colors;
     if (theme == Theme::Light) {
         bg::create("romfs:/background_light.png");
 
-        auto *colors = im::GetStyle().Colors;
-        colors[ImGuiCol_WindowBg]      = ImVec4(1.00f, 0.98f, 0.89f, 1.00f);
-        colors[ImGuiCol_Text]          = ImVec4(0.51f, 0.44f, 0.33f, 1.00f);
+        colors[ImGuiCol_WindowBg]      = ImVec4(1.00f, 0.98f, 0.89f, 0.75f);
         colors[ImGuiCol_TitleBgActive] = ImVec4(0.94f, 0.62f, 0.24f, 1.00f);
+        colors[ImGuiCol_FrameBg]       = ImVec4(0.94f, 0.92f, 0.84f, 1.00f);
+        colors[ImGuiCol_Text]          = ImVec4(0.51f, 0.44f, 0.33f, 1.00f);
 
         text_def_col = 0xff547082;
-        text_cur_col = 0xffac815e;
-        text_min_col = 0xff6a61bf;
-        text_max_col = 0xff8cbea3;
+        text_cur_col = 0xfffd9239;
+        text_min_col = 0xff7573ff;
+        text_max_col = 0xff52b949;
     } else {
         bg::create("romfs:/background_dark.png");
 
-        auto *colors = im::GetStyle().Colors;
-        colors[ImGuiCol_WindowBg]      = ImVec4(1.00f, 0.98f, 0.89f, 1.00f);
-        colors[ImGuiCol_Text]          = ImVec4(0.51f, 0.44f, 0.33f, 1.00f);
-        colors[ImGuiCol_TitleBgActive] = ImVec4(0.94f, 0.62f, 0.24f, 1.00f);
+        colors[ImGuiCol_WindowBg]      = ImVec4(0.30f, 0.32f, 0.33f, 0.75f);
+        colors[ImGuiCol_TitleBgActive] = ImVec4(0.15f, 0.16f, 0.16f, 1.00f);
+        colors[ImGuiCol_FrameBg]       = ImVec4(0.27f, 0.28f, 0.29f, 1.00f);
+        colors[ImGuiCol_Text]          = ImVec4(0.93f, 0.95f, 1.00f, 1.00f);
+        colors[ImGuiCol_PlotLines]     = ImVec4(0.93f, 0.95f, 1.00f, 1.00f);
 
-        text_def_col = 0xff547082;
-        text_cur_col = 0xffac815e;
-        text_min_col = 0xff6a61bf;
-        text_max_col = 0xff8cbea3;
+        text_def_col = 0xfffff1ed;
+        text_cur_col = 0xfffd9239;
+        text_min_col = 0xff7573ff;
+        text_max_col = 0xff77d856;
     }
 }
 

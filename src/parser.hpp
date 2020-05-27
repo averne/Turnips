64db7595f31e40543bc748f5f5886befa0aef38a
averne <averne381@gmail.com>
2020-05-27 12:06:33 +0200

    Move metadata to classes

--- src/parser.hpp
+++ src/parser.hpp
@@ -16,6 +16,7 @@ enum class Version: std::size_t {
     V110, V111, V112, V113, V114,
     V120, V121,
     Unknown,
+    Total = Unknown,
 };
 
 struct VersionInfo {
@@ -54,61 +55,26 @@ struct TurnipPrices {
 };
 
 using VisitorSchedule = std::array<std::uint32_t, 7>;
-using VisitorNames    = std::array<std::string,  7>;
-
-namespace impl {
-
-constexpr static std::array versions = {
-    VersionInfo{ 0x67,    0x6f,    2, 0, 2, 0 }, // 1.0.0
-    VersionInfo{ 0x6d,    0x78,    2, 0, 2, 1 }, // 1.1.0
-    VersionInfo{ 0x6d,    0x78,    2, 0, 2, 2 }, // 1.1.1
-    VersionInfo{ 0x6d,    0x78,    2, 0, 2, 3 }, // 1.1.2
-    VersionInfo{ 0x6d,    0x78,    2, 0, 2, 4 }, // 1.1.3
-    VersionInfo{ 0x6d,    0x78,    2, 0, 2, 5 }, // 1.1.4
-    VersionInfo{ 0x20006, 0x20008, 2, 0, 2, 6 }, // 1.2.0
-    VersionInfo{ 0x20006, 0x20008, 2, 0, 2, 7 }, // 1.2.1
-};
-
-constexpr static std::array turnip_offsets = {
-    0x4118C0ul, 0x412060ul, 0x412060ul, 0x412060ul, 0x412060ul, 0x412060ul,
-    0x412060ul, 0x412060ul,
-};
-
-constexpr static std::array visitor_offsets = {
-    0x414f8cul, 0x41572cul, 0x41572cul, 0x41572cul, 0x41572cul, 0x41572cul,
-    0x4159d8ul, 0x4159d8ul,
-};
-
-constexpr static std::array turnip_patterns = {
-    "Fluctuating",
-    "Large spike",
-    "Decreasing",
-    "Small spike",
-};
-
-constexpr static std::array visitor_names = {
-    "None",
-    "Gulliver",
-    "Label",
-    "Saharah",
-    "Wisp",
-    "Mabel",
-    "CJ",
-    "Flick",
-    "Kicks",
-    "Leif",
-    "Redd",
-};
 
 static_assert(sizeof(VersionInfo)     == 0x10 && std::is_standard_layout_v<VersionInfo>);
 static_assert(sizeof(TurnipPrices)    == 0x44 && std::is_standard_layout_v<TurnipPrices>);
 static_assert(sizeof(VisitorSchedule) == 0x1c && std::is_standard_layout_v<VisitorSchedule>);
-static_assert(turnip_offsets.size()  == versions.size());
-static_assert(visitor_offsets.size() == versions.size());
-
-} // namespace impl
 
 class VersionParser {
+    private:
+        constexpr static inline std::array versions = {
+            VersionInfo{ 0x67,    0x6f,    2, 0, 2, 0 }, // 1.0.0
+            VersionInfo{ 0x6d,    0x78,    2, 0, 2, 1 }, // 1.1.0
+            VersionInfo{ 0x6d,    0x78,    2, 0, 2, 2 }, // 1.1.1
+            VersionInfo{ 0x6d,    0x78,    2, 0, 2, 3 }, // 1.1.2
+            VersionInfo{ 0x6d,    0x78,    2, 0, 2, 4 }, // 1.1.3
+            VersionInfo{ 0x6d,    0x78,    2, 0, 2, 5 }, // 1.1.4
+            VersionInfo{ 0x20006, 0x20008, 2, 0, 2, 6 }, // 1.2.0
+            VersionInfo{ 0x20006, 0x20008, 2, 0, 2, 7 }, // 1.2.1
+        };
+
+        static_assert(versions.size() == static_cast<std::size_t>(Version::Total));
+
     private:
         Version version;
 
@@ -127,14 +93,29 @@ class VersionParser {
                 return Version::Unknown;
             }
 
-            for (std::size_t i = 0; i < impl::versions.size(); ++i)
-                if (impl::versions[i] == info)
+            for (std::size_t i = 0; i < this->versions.size(); ++i)
+                if (this->versions[i] == info)
                     return static_cast<Version>(i);
             return Version::Unknown;
         }
 };
 
 class TurnipParser {
+    private:
+        constexpr static inline std::array turnip_offsets = {
+            0x4118C0ul, 0x412060ul, 0x412060ul, 0x412060ul, 0x412060ul, 0x412060ul,
+            0x412060ul, 0x412060ul,
+        };
+
+        constexpr static inline std::array turnip_patterns = {
+            "Fluctuating",
+            "Large spike",
+            "Decreasing",
+            "Small spike",
+        };
+
+        static_assert(turnip_offsets.size() == static_cast<std::size_t>(Version::Total));
+
     public:
         Version      version = {};
         TurnipPrices prices  = {};
@@ -144,12 +125,12 @@ class TurnipParser {
         TurnipParser(Version version, const std::vector<std::uint8_t> &save): version(version), prices(this->get_prices(save)) { }
 
         inline std::string get_pattern() const {
-            return impl::turnip_patterns[this->prices.pattern_type];
+            return this->turnip_patterns[this->prices.pattern_type];
         }
 
     private:
         inline std::size_t get_tp_offset() const {
-            return (this->version != Version::Unknown) ? impl::turnip_offsets[static_cast<std::size_t>(this->version)] : 0ul;
+            return (this->version != Version::Unknown) ? this->turnip_offsets[static_cast<std::size_t>(this->version)] : 0ul;
         }
 
         inline TurnipPrices get_prices(const std::vector<std::uint8_t> &save) const {
@@ -161,6 +142,28 @@ class TurnipParser {
 };
 
 class VisitorParser {
+    private:
+        constexpr static inline std::array visitor_offsets = {
+            0x414f8cul, 0x41572cul, 0x41572cul, 0x41572cul, 0x41572cul, 0x41572cul,
+            0x4159d8ul, 0x4159d8ul,
+        };
+
+        constexpr static inline std::array visitor_names = {
+            "None",
+            "Gulliver",
+            "Label",
+            "Saharah",
+            "Wisp",
+            "Mabel",
+            "CJ",
+            "Flick",
+            "Kicks",
+            "Leif",
+            "Redd",
+        };
+
+        static_assert(visitor_offsets.size() == static_cast<std::size_t>(Version::Total));
+
     public:
         Version         version  = {};
         VisitorSchedule schedule = {};
@@ -169,18 +172,18 @@ class VisitorParser {
         constexpr VisitorParser() = default;
         VisitorParser(Version version, const std::vector<std::uint8_t> &save): version(version), schedule(this->get_schedule((save))) { }
 
-        inline VisitorNames get_visitor_names() const {
-            VisitorNames names;
+        inline std::array<std::string, 7> get_visitor_names() const {
+            std::array<std::string, 7> names;
             std::transform(this->schedule.begin(), this->schedule.end(), names.begin(),
-            [](std::uint32_t visitor) {
-                return impl::visitor_names[visitor];
+            [this](std::uint32_t visitor) {
+                return this->visitor_names[visitor];
             });
             return names;
         }
 
     private:
         inline std::size_t get_vs_offset() const {
-            return (this->version != Version::Unknown) ? impl::visitor_offsets[static_cast<std::size_t>(this->version)] : 0ul;
+            return (this->version != Version::Unknown) ? this->visitor_offsets[static_cast<std::size_t>(this->version)] : 0ul;
         }
 
         inline VisitorSchedule get_schedule(const std::vector<std::uint8_t> &save) const {

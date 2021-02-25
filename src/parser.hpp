e666ead404facb98bd7b9cf12cf15b2b19da6ce0
Marlon Pontes <marlonpontess@gmail.com>
2021-02-25 13:51:02 -0300

    added Brazilian Portuguese language file, and update to 1.8 offsets

--- src/parser.hpp
+++ src/parser.hpp
@@ -38,6 +38,7 @@ enum class Version: std::size_t {
     V150, V151,
     V160,
     V170,
+    V180,
     Unknown,
     Total = Unknown,
 };
@@ -121,6 +122,7 @@ class VersionParser {
             VersionInfo{ 0x60001, 0x6000c, 2, 0, 2, 14 }, // 1.5.1
             VersionInfo{ 0x70001, 0x70006, 2, 0, 2, 15 }, // 1.6.0
             VersionInfo{ 0x74001, 0x74005, 2, 0, 2, 16 }, // 1.7.0
+            VersionInfo{ 0x78001, 0x78001, 2, 0, 2, 17 }, // 1.8.0
         };
 
         static_assert(versions.size() == static_cast<std::size_t>(Version::Total));
@@ -161,6 +163,7 @@ class TurnipParser {
             0x41d4a0ul, 0x41d4a0ul,                                     // 1.5.x
             0x41d570ul,                                                 // 1.6.0
             0x41b63cul,                                                 // 1.7.0
+            0x41b63cul,                                                 // 1.8.0
         };
 
         constexpr static std::array turnip_patterns = {
@@ -208,6 +211,7 @@ class VisitorParser {
             0x420e18ul, 0x420e18ul,                                     // 1.5.x
             0x420ee8ul,                                                 // 1.6.0
             0x41f0b4ul,                                                 // 1.7.0
+            0x41f0b4ul,                                                 // 1.8.0
         };
 
         constexpr static std::array visitor_names = {
@@ -277,6 +281,7 @@ class DateParser {
             0xb20468ul, 0xb20468ul,                                     // 1.5.x
             0xb25038ul,                                                 // 1.6.0
             0x849388ul,                                                 // 1.7.0
+            0x849388ul,                                                 // 1.8.0
         };
 
         static_assert(date_offsets.size() == static_cast<std::size_t>(Version::Total));
@@ -319,6 +324,7 @@ class WeatherSeedParser {
             0x1e24d4ul, 0x1e24d4ul,                                     // 1.5.x
             0x1e24d4ul,                                                 // 1.6.0
             0x1e24d4ul,                                                 // 1.7.0
+            0x1e24d4ul,                                                 // 1.8.0
         };
 
         constexpr static std::uint32_t weather_seed_max = 2147483647;
